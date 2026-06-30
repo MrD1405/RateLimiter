d14be3721a44b3ef94fdb7c97426d900e14216df
Dushyant <98117205+MrD1405@users.noreply.github.com>
2026-06-30 20:59:04 +0530

    feat : Check if user exists

--- authentication.py
+++ authentication.py
@@ -21,32 +21,40 @@ def authenticate(f):
         if auth_header and auth_header.split(' ')[1]:
             token= auth_header.split(' ')[1]
         if not token:
+            print('No Token')
             return jsonify({'message':'No token'}),401
         try :
             data = jwt.decode(token , os.getenv('JWT_SECRET_KEY') , algorithms =[ os.getenv('SIGNING_ALGORITHM')])
         except jwt.ExpiredSignatureError:
+            print('Expired token')
             return jsonify({'message':'Token has expired'}) , 401
-        except jwt.InValidTokenError:
+        except jwt.InvalidTokenError:
+            print('Invalid Token')
             return jsonify({'message':'Invalid token'}) , 401
-        
-        return f(data['username'] , *args , **kwargs)
+        if not doesUserExist(data['username']):
+            print('User not existing')
+            return jsonify({'message':'User doesnt exist'}) , 401
+        return f(username=data['username'] , *args , **kwargs)
             
     return decorator
 def loginUser(username , password):
-    database.make_db_connection()
-    database.cursor.execute('SELECT EXISTS (SELECT 1 FROM users WHERE username = %s)', (username,))
-    is_user_existing = database.cursor.fetchone()[0]
-    if is_user_existing:
+    if doesUserExist(username):
         raise UsernameAlreadyExistsError
     else:
+        database.make_db_connection()
         database.cursor.execute('INSERT INTO users (username , password) VALUES (%s , %s)', (username , password))
         database.conn.commit()
         database.end_db_connection()
     payload={'username':username , 'password':password , 'iat': datetime.now(timezone.utc)}
     token=jwt.encode(payload ,os.getenv('JWT_SECRET_KEY') , algorithm = os.getenv('SIGNING_ALGORITHM') )
-    print(token)
     return jsonify({'token':token}) , 200
     
+def doesUserExist(username):
+    database.make_db_connection()
+    database.cursor.execute('SELECT EXISTS (SELECT 1 FROM users WHERE username = %s)', (username,))
+    is_user_existing = database.cursor.fetchone()[0]   
+    database.end_db_connection() 
+    return is_user_existing
         
         
     
