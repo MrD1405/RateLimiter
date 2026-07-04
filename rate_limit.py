da9a84ddee8e7da1eefd0353feb7a2ae67c7b2d4
Dushyant <98117205+MrD1405@users.noreply.github.com>
2026-07-04 22:21:07 +0530

    fix : Fixed Window as a component

--- rate_limit.py
+++ rate_limit.py
@@ -7,6 +7,10 @@ from database import Database , Redis
 from datetime import datetime
 
 load_dotenv()
+
+
+class RateLimitError(Exception):
+    pass
 database = Database()
 redis = Redis()
 def rate_limit(f):
@@ -14,19 +18,26 @@ def rate_limit(f):
     def decorator(*args , **kwargs):
         token=''
         username=kwargs['username']
-        redis.make_connection()
-        r=redis.conn
-        now=datetime.now()
-        now=now.strftime("%Y-%m-%d %H:%M")
-        username=f"username:{now}"
-        userVisitCounter=r.get(username)
-        userVisitCounter = int(userVisitCounter) if userVisitCounter else userVisitCounter
-        if userVisitCounter == None:
-            r.set(username,100,ex=60)
-        elif userVisitCounter <=0 :
+        try:
+            fixedWindow(username)
+        except RateLimitError:
             return jsonify({'message':'Exceeded Rate Limit'}) , 429
-        else:
-            r.decr(username)
         return f(*args , **kwargs)
     return decorator
+
+def fixedWindow(username):
+    now=datetime.now()
+    now=now.strftime("%Y-%m-%d %H:%M")
+    username=f"{username}:{now}"
+    redis.make_connection()
+    r=redis.conn
+    userVisitCounter=r.get(username)
+    userVisitCounter = int(userVisitCounter) if userVisitCounter else userVisitCounter
+    if userVisitCounter == None:
+        r.set(username,100,ex=60)
+    elif userVisitCounter <=0 :
+        raise RateLimitError 
+    else:
+        r.decr(username)
+    redis.end_connection()
             
